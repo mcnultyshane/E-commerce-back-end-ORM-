58e1cc655c8942de2cb4067b12a9a9cb0488bc9b
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-07-09 00:23:37 -0400

    working on Category model and server.js.  imported sequelize.

--- models/Category.js
+++ models/Category.js
@@ -4,9 +4,28 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
+    // * `Category`
 Category.init(
   {
     // define columns
+    //   * `id`
+    id: {
+// * Integer.
+      type: DataTypes.INTEGER,
+// * Doesn't allow null values.      
+      allowNull: false,
+// * Set as primary key.      
+      primaryKey: true,
+// * Uses auto increment.      
+      autoIncrement: true,
+    },
+// * `category_name`
+    category_name: {
+// * String.      
+      type: DataTypes.STRING,
+// * Doesn't allow null values.      
+      allowNull: false,
+    }
   },
   {
     sequelize,
@@ -20,12 +39,3 @@ Category.init(
 module.exports = Category;
 
 
-// * `Category`
-//   * `id`
-// * Integer.
-// * Doesn't allow null values.
-// * Set as primary key.
-// * Uses auto increment.
-// * `category_name`
-// * String.
-// * Doesn't allow null values.
--- models/index.js
+++ models/index.js
@@ -7,11 +7,20 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 
 // Categories have many Products
+// (as a category can have multiple products but a produc can only belong to one category)
 
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
 
+//=============================================================
+// (Allow products to have multiple tags and tags to have many products by using the `ProductTag` through model.)
+//=============================================================== 
+
+
+// ===========================================================
+// > **Hint:** Make sure you set up foreign key relationships that match the column we created in the respective models.
+// ============================================================
 module.exports = {
   Product,
   Category,
